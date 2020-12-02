25ba56c2fe7766ca18675b075a04cdb8a9dec4c2
sim1-avo <56638877+sim1-avo@users.noreply.github.com>
2020-12-02 12:38:53 +0100

    interfaccia Visitor e la sua implementazione

---
+++ src/GenerateXML.java
@@ -0,0 +1,162 @@
+public class GenerateXML implements Visitor{
+
+    @Override
+    public Object visit(ProgramOP p) {
+        return null;
+    }
+
+    @Override
+    public Object visit(AndOP a) {
+        return null;
+    }
+
+    @Override
+    public Object visit(AssignOP a) {
+        return null;
+    }
+
+    @Override
+    public Object visit(BodyOP b) {
+        return null;
+    }
+
+    @Override
+    public Object visit(CallProcOP c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(DivOP c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ElifOP c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ElseOP c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(EqualsOP c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(Expr c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(GreaterEqualsOP c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(GreaterThanOP c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(Id c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(IdListInitOP c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(IfOP c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(LessEqualsOP c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(LessThanOP c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(MinusOP c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(NotEqualsOP c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(NotOP c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(OrOP c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ParDeclOP c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ProcBodyOP c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ProcOP c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ReadOP c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(ReturnExprsOP c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(Stat c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(TimesOP c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(UMinusOP c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(VarDeclOP c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(WhileOP c) {
+        return null;
+    }
+
+    @Override
+    public Object visit(WriteOP c) {
+        return null;
+    }
+}
